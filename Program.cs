c38224e2730b895086cb82c3921e42543bfcc14f
Ankit-Kumar4 <113576375+Ankit-Kumar4@users.noreply.github.com>
2023-09-26 00:05:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INDEXOUT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                int[] arr = new int[3];
+                arr[0] = 1;
+                arr[1] = 2;
+                arr[3] = 4;
+                arr[4] = 5;
+                foreach (int i in arr)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine();
+            }
+
+
+            catch (IndexOutOfRangeException ex) {
+                Console.WriteLine("Array range is exceeded......");
+                Console.ReadLine();
+            }
+
+           
+
+        }
+    }
+}
